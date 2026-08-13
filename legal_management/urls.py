610c5c0bc69aa98b98b8010f9602452e904da086
Mutee141 <mutee141@example.com>
2026-08-13 22:00:37 +0500

    fix: explicitly wire up media file serving for production (static() helper ignores media when DEBUG=False)

--- legal_management/urls.py
+++ legal_management/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from cases.views import DashboardView
 
 urlpatterns = [
@@ -15,7 +16,9 @@ urlpatterns = [
 ]
 
 # Always serve media files (needed for production on Render)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
